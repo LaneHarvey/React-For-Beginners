1d4aa087251a2bfcf8dcfbed1abef557dca901dc
Lane Harvey <mrlaneharvey@gmail.com>
2017-03-03 12:13:42 -0700

    css imported

--- catch-of-the-day/src/components/StorePicker.jsx
+++ catch-of-the-day/src/components/StorePicker.jsx
@@ -6,7 +6,7 @@ class StorePicker extends React.Component {
       <form className="sore-selector">
         {/* Hello */}
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" required placeholder="Store Name"/>
         <button type="submit">Visit Store</button>
       </form>
     )
