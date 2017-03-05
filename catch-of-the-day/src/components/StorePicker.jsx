aed53529a939474bcc844137b8511bf409e45998
Lane Harvey <mrlaneharvey@gmail.com>
2017-03-04 17:28:18 -0700

    onSubmit added arrow function

--- catch-of-the-day/src/components/StorePicker.jsx
+++ catch-of-the-day/src/components/StorePicker.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
-  goToStore() {
+  // constructor() {
+  //   super();
+  //   this.goToStore= this.goToStore.bind(this);
+  // }
+  goToStore(event) {
     event.preventDefault();
     console.log('You changed the URL');
-      // first grab the text from the box
-      const value = $('input').val();
-      // second we're going to transition from / to /store/:storeId
+    // first grab the text from the box
+    console.log(this.storeInput.value);
+    // second we're going to transition from / to /store/:storeId
   }
 
   render() {
     // Any where else
     return (
-      <form className="store-selector" onSubmit={this.goToStore}>
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         <h2>Please Enter a Store</h2>
         <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input}} />
         <button type="submit">Visit Store</button>
