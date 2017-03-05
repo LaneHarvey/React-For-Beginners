efaa66dc28d6f09c8480d405bc139c8487f89b56
Lane Harvey <mrlaneharvey@gmail.com>
2017-03-04 17:20:51 -0700

    const value added and goToStore function

--- catch-of-the-day/src/components/StorePicker.jsx
+++ catch-of-the-day/src/components/StorePicker.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
+  goToStore() {
+    event.preventDefault();
+    console.log('You changed the URL');
+      // first grab the text from the box
+      const value = $('input').val();
+      // second we're going to transition from / to /store/:storeId
+  }
+
   render() {
+    // Any where else
     return (
-      <form className="sore-selector">
-        {/* Hello */}
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input}} />
         <button type="submit">Visit Store</button>
       </form>
     )
